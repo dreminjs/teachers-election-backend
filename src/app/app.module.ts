470d3c3b6bbc6c81a98538dd13542e85bb4d3993
Andrey Dremin <sankoa230@gmail.com>
2024-12-20 22:34:01 +0300

    feat: serve static

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { PasswordModule } from 'src/password';
 import { TeacherModule } from 'src/teacher/';
 import { ConfigModule } from '@nestjs/config';
 import { TeacherReviewModule } from 'src/teacher-review';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -16,7 +18,10 @@ import { TeacherReviewModule } from 'src/teacher-review';
     PasswordModule,
     ConfigModule,
     TeacherModule,
-    TeacherReviewModule
+    TeacherReviewModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', "images"),
+    }),
   ],
   controllers: [AppController],
 })
