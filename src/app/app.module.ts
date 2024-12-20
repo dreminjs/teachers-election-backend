4c6e58af43b62e35b572a44269ba076b6afac997
Andrey Dremin <sankoa230@gmail.com>
2024-12-20 21:09:55 +0300

    fix: app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth';
 import { PasswordModule } from 'src/password';
 import { TeacherModule } from 'src/teacher/';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, PrismaModule, AuthModule, PasswordModule, TeacherModule],
+  imports: [
+    UserModule,
+    PrismaModule,
+    AuthModule,
+    PasswordModule,
+    ConfigModule,
+    TeacherModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,10 +6,18 @@ import { UserModule } from 'src/user/user.module';
 import { PasswordModule } from 'src/password';
 import { TokenModule } from 'src/token';
 import { AccessTokenStrategy } from './strategies/access-token.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UserModule, PasswordModule, TokenModule],
-  providers: [AuthService,AccessTokenStrategy],
-  controllers: [AuthController,AccessTokenStrategy],
+  imports: [
+    PrismaModule,
+    UserModule,
+    PasswordModule,
+    TokenModule,
+    ConfigModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, AccessTokenStrategy],
+  exports: [AccessTokenStrategy],
 })
 export class AuthModule {}
