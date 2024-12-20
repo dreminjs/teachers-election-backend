c95e656bb955a6798f39735e0bb5a93d343da474
Andrey Dremin <sankoa230@gmail.com>
2024-12-20 21:44:55 +0300

    fix: changed text

--- src/auth/guards/signup.guard.ts
+++ src/auth/guards/signup.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { SignupDto } from '../dto/signup.dto';
 import { UserService } from 'src/user';
@@ -14,11 +14,11 @@ export class SignupGuard implements CanActivate {
     const user = await this.userService.findOne({ email });
 
     if (user) {
-      return false;
+      throw new HttpException('Такой пользователь уже существует', HttpStatus.BAD_REQUEST)
     }
 
     if(codeWord !== "1") {
-      return false;
+      throw new HttpException('Не правильная секретная информация', HttpStatus.BAD_REQUEST)
     }
     
     return true;
