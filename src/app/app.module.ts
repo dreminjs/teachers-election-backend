71c188f9c8c12b4a412fbc2d09301ebc050a80dc
Andrey Dremin <sankoa230@gmail.com>
2024-12-20 23:20:12 +0300

    one more fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { join } from 'path';
     TeacherModule,
     TeacherReviewModule,
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..', "images"),
+      rootPath: join(__dirname, '..', '..',"..", "images"),
     }),
   ],
   controllers: [AppController],
