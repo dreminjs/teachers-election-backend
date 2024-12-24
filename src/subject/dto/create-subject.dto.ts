076ce5d42ecc5ece473228fa71524eb3c53f0b2b
Andrey Dremin <sankoa230@gmail.com>
2024-12-24 23:36:42 +0300

    fix: create subject dto decorators

--- src/subject/dto/create-subject.dto.ts
+++ src/subject/dto/create-subject.dto.ts
@@ -1,8 +1,9 @@
-
-
-
-
+import { IsNotEmpty, IsString, Max, MaxLength, Min } from 'class-validator';
 
 export class CreateSubjectDto {
-    title: string
+  @IsString()
+  @IsNotEmpty({ message: 'Название предмета не может быть пустым' })
+  @Min(3, { message: 'Название предмета должно быть не менее 3 символов' })
+  @MaxLength(100, { message: 'Название предмета не должно превышать 100 символов' })
+  title: string;
 }
