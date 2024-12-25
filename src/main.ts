a79bff1d5a20d1cce2eb2f37ca9d17b134e720d7
Andrey Dremin <sankoa230@gmail.com>
2024-12-25 19:17:38 +0300

    one more fix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import * as cookieParser from 'cookie-parser';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,10 @@ async function bootstrap() {
   app.enableCors({ credentials: true, origin: '*' });
 
   app.use(cookieParser());
+
+  const logger = new Logger();
+
+  logger.log('port: ', 3000);
 
   await app.listen(3000);
 }
