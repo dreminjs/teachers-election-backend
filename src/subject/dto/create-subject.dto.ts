4d7fe1c5e4cecd4c9624431129c55837300b5d0c
Andrey Dremin <sankoa230@gmail.com>
2024-12-30 20:40:10 +0300

    one more fix

--- src/subject/dto/create-subject.dto.ts
+++ src/subject/dto/create-subject.dto.ts
@@ -1,9 +1,9 @@
-import { IsNotEmpty, IsString, Max, MaxLength, Min } from 'class-validator';
+import { IsNotEmpty, IsString, Max, MaxLength, Min, MinLength } from 'class-validator';
 
 export class CreateSubjectDto {
   @IsString()
   @IsNotEmpty({ message: 'Название предмета не может быть пустым' })
-  @Min(3, { message: 'Название предмета должно быть не менее 3 символов' })
+  @MinLength(3, { message: 'Название предмета должно быть не менее 3 символов' })
   @MaxLength(100, { message: 'Название предмета не должно превышать 100 символов' })
   title: string;
 }
