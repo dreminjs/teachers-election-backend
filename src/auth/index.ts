61531082f116ac6bbfc1a22d5a2ba0f50f8dee23
andrey_dremin <sankoa230@gmail.com>
2025-01-13 15:06:56 +0300

    one more change

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,4 +1,3 @@
 export { AuthController } from './auth.controller';
-export { AuthService } from './auth.service';
 export { AuthModule } from './auth.module';
 export { AccessTokenGuard } from '../token/guards/access-token.guard';
