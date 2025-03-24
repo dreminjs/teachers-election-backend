ef9137a162d92618efbef30302a3f3bf04b68109
andrey_dremin <sankoa230@gmail.com>
2025-03-24 17:36:26 +0300

    chore: delete token during sign in

--- src/auth/guards/signin.guard.ts
+++ src/auth/guards/signin.guard.ts
@@ -7,17 +7,22 @@ import {
 } from '@nestjs/common';
 import { UserService } from 'src/user';
 import { comparePassword } from '../helpers/password.helpers';
+import { TokenService } from 'src/token';
 
 @Injectable()
 export class SigninGuard implements CanActivate {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly tokenService: TokenService
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const { email, password } = context.switchToHttp().getRequest().body;
 
-    const { password: hashPassword } = await this.userService.findOne({
-      where: { email },
-    });
+    const { password: hashPassword, id: userId } =
+      await this.userService.findOne({
+        where: { email },
+      });
 
     if (!hashPassword)
       throw new HttpException(
@@ -25,10 +30,17 @@ export class SigninGuard implements CanActivate {
         HttpStatus.BAD_REQUEST
       );
 
-    const isPasswordValid = await comparePassword({
+    const deleteTokenQuery = this.tokenService.deleteOne({ userId });
+
+    const isPasswordValidQuery = comparePassword({
       password,
       hashPassword,
     });
+
+    const [_, isPasswordValid] = await Promise.all([
+      deleteTokenQuery,
+      isPasswordValidQuery,
+    ]);
 
     if (!isPasswordValid) {
       throw new HttpException('Неверная пара пароль', HttpStatus.BAD_REQUEST);
